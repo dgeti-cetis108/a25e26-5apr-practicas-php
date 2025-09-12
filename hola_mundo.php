4ed4f578d8be69cdce5abe9f31a32301f5f700e9
Bidkar Aragón Cárdenas <bidkar@cetis108.edu.mx>
2025-09-12 08:29:48 -0700

    Configuración y prueba inicial

---
+++ hola_mundo.php
@@ -0,0 +1,6 @@
+<?php
+echo "Hola mundo desde PHP";
+// C# Console.WriteLine("Hola Mundo"); python print("Hola Mundo")
+
+// En archivos con puro código PHP, se recomienda no cerrar
+// la etiqueta php y dejar una línea en blanco al final
