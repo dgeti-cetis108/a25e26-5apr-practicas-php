7fc57370a5923b5b185057c6e931fe6e029b6aa9
Bidkar Aragón Cárdenas <bidkar@cetis108.edu.mx>
2025-09-24 07:50:22 -0700

    Add suma_numeros.php to perform addition of two fixed numbers

---
+++ suma_numeros.php
@@ -0,0 +1,12 @@
+<?php
+// snake_case
+/**
+ * Realiza un programa que dados dos números fijos
+ * haga la operación de sumatoria de ambos y muestre
+ * el resultado.
+ * . es el operador de concatenación en PHP
+ */
+$num1 = 5;
+$num2 = 82;
+
+echo "$num1+$num2=" . ($num1 + $num2);
