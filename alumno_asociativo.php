d339ff20c491f59ca1174150bfd96c64d0b411ba
Bidkar Aragón Cárdenas <bidkar@cetis108.edu.mx>
2025-10-22 07:50:10 -0700

    Agregar archivo PHP alumno_asociativo.php con ejemplos de uso de arreglos y saludo

---
+++ alumno_asociativo.php
@@ -0,0 +1,13 @@
+<?php
+
+$aalumno = ['Alberto', 'Villa'];
+
+
+echo "Hola " . $aalumno[0];
+
+$_alumno = [
+    'nombre' => 'Cesar',
+    'apellidos' => 'Pollorena'
+];
+
+echo "Hola " . $_alumno['nombre'];
